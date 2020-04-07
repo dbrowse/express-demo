c36210944c62e07e00dbc41bde8672dfe145dcad
dgeorgiev <dgeorgiev_job@yahoo.com>
2020-04-07 19:49:13 +0300

    Post requst with Joi validation

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
+
+//add middleware to use express when request
+app.use(express.json());
 
 const courses = [
 	{ id: 1, name: "course1" },
 	{ id: 2, name: "course2" },
-	{ id: 3, name: "course3" }
+	{ id: 3, name: "course3" },
 ];
 
 app.get("/", (req, res) => {
@@ -15,9 +19,41 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.get("/api/courses/:id", (req, res) => {
-	const course = courses.find(c => c.id === parseInt(req.params.id));
+	const course = courses.find((c) => c.id === parseInt(req.params.id));
 	if (!course)
 		res.status(404).send("The course with the given ID was not found");
+	res.send(course);
+});
+app.get("/api/courses/:id", (req, res) => {
+	const course = courses.find((c) => c.id === parseInt(req.params.id));
+	if (!course)
+		res.status(404).send("The course with the given ID was not found");
+	res.send(course);
+});
+app.get("/api/courses/:id", (req, res) => {
+	const course = courses.find((c) => c.id === parseInt(req.params.id));
+	if (!course)
+		res.status(404).send("The course with the given ID was not found");
+	res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+	const schema = {
+		name: Joi.string().min(3).required(),
+	};
+
+	const result = Joi.validate(req.body, schema);
+
+	if (result.error) {
+		req.status(400).send(result.error);
+		return;
+	}
+
+	const course = {
+		id: courses.length + 1,
+		name: req.body.name,
+	};
+	courses.push(course);
 	res.send(course);
 });
 
